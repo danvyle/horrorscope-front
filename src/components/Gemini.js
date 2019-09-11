0565b3d282843f7122c94bbbe18f5305c66cf182
Dan-Vy Le <github email address>
2019-09-11 14:24:41 -0700

    self help page for all signs

--- src/components/Gemini.js
+++ src/components/Gemini.js
@@ -12,7 +12,8 @@ class Gemini extends React.Component {
     super()
     this.state = {
       horoscopes: [],
-      retrogrades: []
+      retrogrades: [],
+      image: gemimg
     }
   }
   componentDidMount() {
@@ -68,28 +69,30 @@ class Gemini extends React.Component {
   render() {
     if (!this.retrogradeCheck() && !this.whatMoonPhase()) {
       return(
-        <div className ="App">
-          <header className="App-header">
-            <SelfHelp />
-          </header>
+        <div>
+          <SelfHelp image = {this.state.image}/>
         </div>
       )
     } else {
       return (
-        <div className= 'horoscope-display'>
-          <div onClick = {this.handleClickHome} className="horoscope-img"> <h3>HOME</h3> <img src = {gemimg}/></div>
-          <div className= "heading-card">
-            <h1 > HORRORSCOPES </h1>
-            <h2> LOOKS LIKE THE UNIVERSE IS TO BLAME </h2>
-          </div>
-          <div className="retrogrades"><Retrogrades retrogrades = {this.state.retrogrades}/></div>
-          <div className="moon"><Moon /></div>
-          <div className="horoscope"><Horoscope horoscopes = {this.state.horoscopes}/></div>
+        <div className ="App">
+          <header className="App-header">
+            <div className= 'horoscope-display'>
+              <div onClick = {this.handleClickHome} className="horoscope-img"> <h3>HOME</h3> <img src = {gemimg}/></div>
+              <div className= "heading-card">
+                <h1 > HORRORSCOPES </h1>
+                <h2> LOOKS LIKE THE UNIVERSE IS TO BLAME </h2>
+              </div>
+              <div className="retrogrades"><Retrogrades retrogrades = {this.state.retrogrades}/></div>
+              <div className="moon"><Moon /></div>
+              <div className="horoscope"><Horoscope horoscopes = {this.state.horoscopes}/></div>
+            </div>
+          </header>
         </div>
       );
 
     }
   }
-}  
+}
 
-  export default Gemini;
+export default Gemini;
